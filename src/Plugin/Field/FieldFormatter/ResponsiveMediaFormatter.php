5ad7860e87fa858e224aed173c239ae18ed73140
Alexey Sivets <alexey.sivets@colours.nl>
2018-11-30 11:15:40 +0100

    Add module files

---
+++ src/Plugin/Field/FieldFormatter/ResponsiveMediaFormatter.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Drupal\responsive_media_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\image\ImageStyleStorageInterface;
+use Drupal\media\Plugin\Field\FieldFormatter\MediaThumbnailFormatter;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\media\MediaInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
+use Drupal\Core\Url;
+use Drupal\responsive_image\Entity\ResponsiveImageStyle;
+use Drupal\Core\Cache\Cache;
+use Drupal\file\Entity\File;
+
+/**
+ * Plugin implementation of the 'responsive_media' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "responsive_media",
+ *   label = @Translation("Responsive media"),
+ *   field_types = {
+ *     "entity_reference"
+ *   }
+ * )
+ */
+class ResponsiveMediaFormatter extends MediaThumbnailFormatter {
+
+  /**
+   * Responsive Image Style Storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $responsiveImageStyleStorage;
+
+  /**
+   * The image style entity storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $imageStyleStorage;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The link generator.
+   *
+   * @var \Drupal\Core\Utility\LinkGeneratorInterface
+   */
+  protected $linkGenerator;
+
+  /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs an ResponsiveMediaFormatter object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\image\ImageStyleStorageInterface $image_style_storage
+   *   The image style entity storage handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $responsive_image_style_storage
+   *   The responsive image style storage.
+   * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
+   *   The link generator service.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, ImageStyleStorageInterface $image_style_storage, RendererInterface $renderer, EntityStorageInterface $responsive_image_style_storage, LinkGeneratorInterface $link_generator) {
+    ImageFormatter::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $current_user, $image_style_storage);
+    $this->renderer = $renderer;
+    $this->responsiveImageStyleStorage = $responsive_image_style_storage;
+    $this->imageStyleStorage = $image_style_storage;
+    $this->linkGenerator = $link_generator;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('current_user'),
+      $container->get('entity_type.manager')->getStorage('image_style'),
+      $container->get('renderer'),
+      $container->get('entity.manager')->getStorage('responsive_image_style'),
+      $container->get('link_generator')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'responsive_image_style' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * This has to be overridden because FileFormatterBase expects $item to be
+   * of type \Drupal\file\Plugin\Field\FieldType\FileItem and calls
+   * isDisplayed() which is not in FieldItemInterface.
+   */
+  protected function needsEntityLoad(EntityReferenceItem $item) {
+    return !$item->hasNewEntity();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $responsive_image_options = [];
+    $responsive_image_styles = $this->responsiveImageStyleStorage->loadMultiple();
+    if ($responsive_image_styles && !empty($responsive_image_styles)) {
+      foreach ($responsive_image_styles as $machine_name => $responsive_image_style) {
+        if ($responsive_image_style->hasImageStyleMappings()) {
+          $responsive_image_options[$machine_name] = $responsive_image_style->label();
+        }
+      }
+    }
+
+    $elements['responsive_image_style'] = [
+      '#title' => t('Responsive image style'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('responsive_image_style') ?: NULL,
+      '#required' => TRUE,
+      '#options' => $responsive_image_options,
+      '#description' => [
+        '#markup' => $this->linkGenerator->generate($this->t('Configure Responsive Image Styles'), new Url('entity.responsive_image_style.collection')),
+        '#access' => $this->currentUser->hasPermission('administer responsive image styles'),
+      ],
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    $responsive_image_style = $this->responsiveImageStyleStorage->load($this->getSetting('responsive_image_style'));
+    if ($responsive_image_style) {
+      $summary[] = t('Responsive image style: @responsive_image_style', ['@responsive_image_style' => $responsive_image_style->label()]);
+    }
+    else {
+      $summary[] = t('Select a responsive image style.');
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+
+    $elements = [];
+    $media_items = $this->getEntitiesToView($items, $langcode);
+
+    if (empty($media_items)) {
+      return $elements;
+    }
+
+    // Collect cache tags to be added for each item in the field.
+    $responsive_image_style = $this->responsiveImageStyleStorage->load($this->getSetting('responsive_image_style'));
+    $image_styles_to_load = [];
+    $cache_tags = [];
+    if ($responsive_image_style) {
+      $cache_tags = Cache::mergeTags($cache_tags, $responsive_image_style->getCacheTags());
+      $image_styles_to_load = $responsive_image_style->getImageStyleIds();
+    }
+
+    $image_styles = $this->imageStyleStorage->loadMultiple($image_styles_to_load);
+    foreach ($image_styles as $image_style) {
+      $cache_tags = Cache::mergeTags($cache_tags, $image_style->getCacheTags());
+    }
+
+    /** @var \Drupal\media\MediaInterface[] $media_items */
+    foreach ($media_items as $delta => $media) {
+
+      $media_field = $media->get('field_media_image')->first()->getValue();
+      $file = File::load($media_field['target_id']);
+
+      $elements[$delta] = [
+        '#theme' => 'responsive_image',
+        '#attributes' => [
+          'alt' => $media_field['alt'],
+        ],
+        '#width' => $media_field['width'],
+        '#height' => $media_field['height'],
+        '#responsive_image_style_id' => $this->getSetting('responsive_image_style'),
+        '#uri' => $file->getFileUri(),
+        '#cache' => [
+          'tags' => $cache_tags,
+        ],
+      ];
+    }
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    $dependencies = parent::calculateDependencies();
+    $style_id = $this->getSetting('responsive_image_style');
+    /** @var \Drupal\responsive_image\ResponsiveImageStyleInterface $style */
+    if ($style_id && $style = ResponsiveImageStyle::load($style_id)) {
+      // Add the responsive image style as dependency.
+      $dependencies[$style->getConfigDependencyKey()][] = $style->getConfigDependencyName();
+    }
+    return $dependencies;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(FieldDefinitionInterface $field_definition) {
+    // This formatter is only available for entity types that reference
+    // media items.
+    return ($field_definition->getFieldStorageDefinition()->getSetting('target_type') == 'media');
+  }
+
+  /**
+   * Get the URL for the media thumbnail.
+   *
+   * @param \Drupal\media\MediaInterface $media
+   *   The media item.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity that the field belongs to.
+   *
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   *
+   * @return \Drupal\Core\Url|null
+   *   The URL object for the media item or null if we don't want to add
+   *   a link.
+   */
+  protected function getMediaThumbnailUrl(MediaInterface $media, EntityInterface $entity) {
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      if (!$entity->isNew()) {
+        $url = $entity->toUrl();
+      }
+    }
+    elseif ($image_link_setting === 'media') {
+      $url = $media->toUrl();
+    }
+    return $url;
+  }
+
+}
